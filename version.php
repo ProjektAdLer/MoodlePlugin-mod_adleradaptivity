b9590572fb2681a8f524c915b054ca4227b0f6ac
Markus Heck <markus.heck@hs-kempten.de>
2023-11-12 15:22:18 +0100

    Update version to '2.0.0' and raise required moodle version to 4.1.0 (we aren't supporting older moodle versions already for some time)

--- version.php
+++ version.php
@@ -8,8 +8,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023110800;
-$plugin->requires = 2021051712.05;  // Moodle version
-$plugin->release = '2.0.0-rc.3';
+$plugin->version = 2023111200;
+$plugin->requires = 2022112800;  // Moodle version
+$plugin->release = '2.0.0';
 $plugin->component = 'mod_adleradaptivity'; // Full name of the plugin (used for diagnostics)
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_STABLE;
