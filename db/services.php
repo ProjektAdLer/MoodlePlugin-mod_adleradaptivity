4d07956bb1edee58491915ccf189916ae62ec1a9
Markus Heck <markus.heck@hs-kempten.de>
2023-11-14 19:09:47 +0100

    remove MOODLE_MOBILE_SERVICES support

--- db/services.php
+++ db/services.php
@@ -7,7 +7,7 @@ $functions = array(
         'description' => 'Get details for all questions in one learning element',    //human readable description of the web service function
         'type' => 'read',                  //database rights of the web service function (read, write)
         'ajax' => false,        // is the service available to 'internal' ajax calls.
-        'services' => array(MOODLE_OFFICIAL_MOBILE_SERVICE, 'adler_services'),   // Optional, only available for Moodle 3.1 onwards. List of built-in services (by shortname) where the function will be included.  Services created manually via the Moodle interface are not supported.
+        'services' => array('adler_services'),   // Optional, only available for Moodle 3.1 onwards. List of built-in services (by shortname) where the function will be included.  Services created manually via the Moodle interface are not supported.
         'capabilities' => 'mod/adleradaptivity:view', // comma separated list of capabilities used by the function.
         'loginrequired' => true
     ),
@@ -16,7 +16,7 @@ $functions = array(
         'description' => 'Get details for all tasks in one learning element',    //human readable description of the web service function
         'type' => 'read',
         'ajax' => false,
-        'services' => array(MOODLE_OFFICIAL_MOBILE_SERVICE, 'adler_services'),
+        'services' => array('adler_services'),
         'capabilities' => 'mod/adleradaptivity:view',
         'loginrequired' => true
     ),
@@ -25,7 +25,7 @@ $functions = array(
         'description' => 'Answer one question',    //human readable description of the web service function
         'type' => 'write',
         'ajax' => false,
-        'services' => array(MOODLE_OFFICIAL_MOBILE_SERVICE, 'adler_services'),
+        'services' => array('adler_services'),
         'capabilities' => 'mod/adleradaptivity:view',
         'loginrequired' => true
     ),
