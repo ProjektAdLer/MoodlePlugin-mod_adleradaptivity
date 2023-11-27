fe947b2de7ade35148f77888d5264163a7072d41
Markus Heck <markus.heck@hs-kempten.de>
2023-11-27 16:42:04 +0100

    add github actions pipeline and experimental support for moodle 4.3

--- version.php
+++ version.php
@@ -8,8 +8,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023112700;
+$plugin->version = 2023112701;
 $plugin->requires = 2022112800;  // Moodle version
 $plugin->release = '2.0.1-dev';
 $plugin->component = 'mod_adleradaptivity'; // Full name of the plugin (used for diagnostics)
-$plugin->maturity = MATURITY_STABLE;
+$plugin->maturity = MATURITY_ALPHA;
