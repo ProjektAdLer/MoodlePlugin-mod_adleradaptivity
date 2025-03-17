2f7a8e943a2a559772278e6034f6ef0cd8a6915c
Markus <markus.heck@hs-kempten.de>
2025-03-17 01:41:09 +0100

    bump version.php

--- version.php
+++ version.php
@@ -8,11 +8,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024102108;
+$plugin->version = 2025031600;
 $plugin->requires = 2024042200;  // Moodle version
-$plugin->release = '3.0.0-dev';
+$plugin->release = '3.1.0';
 $plugin->component = 'mod_adleradaptivity'; // Full name of the plugin (used for diagnostics)
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'local_logging' => ANY_VERSION,
 );
